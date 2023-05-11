b2b1339ee2c82b793791f63f82c6f11f871a0256
Kenny Weiss <kenny1weiss@aol.com>
2023-05-11 13:08:58 -0700

    Save Post to database

--- flask_app/controllers/posts.py
+++ flask_app/controllers/posts.py
@@ -2,8 +2,17 @@ from flask_app import app
 from flask import render_template, redirect, request, url_for, session, flash
 from flask_bcrypt import Bcrypt
 bcrypt = Bcrypt(app)
-# from flask_app.models import post
+from flask_app.models import post
 
 @app.route("/post")
-def post():
+def display_post():
+    return "Posted"
+
+@app.route("/post", methods=["POST"])
+def user_post():
+    data = {
+        "content" : request.form["user_post"],
+        "user_id" : session["id"]
+    }
+    post.Post.create(data)
     return "Posted"
--- flask_app/models/post.py
+++ flask_app/models/post.py
@@ -1,12 +1,18 @@
 from flask import flash
 from flask_app.config.mysqlconnection import connectToMySQL
 
-class User:
-    db = "login_registration"
+class Post:
+    db = "coding_dojo_wall"
     def __init__(self, data):
         self.id = data['id']
-        self.first_name = data['first_name']
-        self.last_name = data['last_name']
-        self.email = data['email']
-        self.password = data['password']
+        self.content = data['content']
         self.created_at = data['created_at']
+        self.updated_at = data['updated_at']
+        self.creator = None
+
+    @classmethod
+    def create(cls,data):
+        query = """INSERT INTO posts (content, user_id) 
+                VALUES (%(content)s, %(user_id)s);"""
+        results = connectToMySQL(cls.db).query_db(query,data)
+        return results
